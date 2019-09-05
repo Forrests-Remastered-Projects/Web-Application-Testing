08a877276f8d86d9d95529dea03f26284bf57620
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-09-05 12:59:58 -0700

    Getting project up and running

---
+++ my-app/src/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each';
+import App from './App.js';
+
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    render(<App />);
+  });
+  it('One Big Test', () => {
+    const {getByText} = render(<App />);
+    const strike = getByText(/^strike$/i);
+    const ball = getByText(/^ball$/i);
+    const foul = getByText(/^foul$/i);
+    const hit = getByText(/^hit$/i);
+
+    fireEvent.click(strike);
+    fireEvent.click(strike);
+    getByText(/Strikes: 2/);
